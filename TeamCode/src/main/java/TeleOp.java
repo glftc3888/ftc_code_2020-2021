37161fd0c75e9df230e7466a8fe74eaf8fcc5109
glftc3888 <greasedlightningrobotics@gmail.com>
2020-09-21 14:49:45 -0700

    setting up the motors

--- TeamCode/src/main/java/TeleOp.java
+++ TeamCode/src/main/java/TeleOp.java
@@ -1,12 +1,14 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOpV2", group = "TeleOp")
 
 abstract class TeleOp extends OpMode {
-    DcMotor topLeft;
-    DcMotor topRight;
-    DcMotor bottomLeft;
-    DcMotor bottomRight;
-
-
-    
+    DcMotor topLeft = hardwareMap.dcMotor.get("Top left motor");
+    DcMotor topRight = hardwareMap.dcMotor.get("Top right motor");
+    DcMotor bottomLeft = hardwareMap.dcMotor.get("Bottom left motor");
+    DcMotor bottomRight = hardwareMap.dcMotor.get("Bottom right motor");
 }
