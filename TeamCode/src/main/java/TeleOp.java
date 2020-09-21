5b344b762010be311e9d634ad1ec59eff9723ff9
glftc3888 <greasedlightningrobotics@gmail.com>
2020-09-21 11:59:09 -0700

    fist commit of the season

--- TeamCode/src/main/java/TeleOp.java
+++ TeamCode/src/main/java/TeleOp.java
@@ -1,5 +1,12 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 abstract class TeleOp extends OpMode {
+    DcMotor topLeft;
+    DcMotor topRight;
+    DcMotor bottomLeft;
+    DcMotor bottomRight;
 
+
+    
 }
