abc96591513a4ad5eea46294e3d94317cf2b5bf1
glftc3888 <greasedlightningrobotics@gmail.com>
2020-09-25 11:27:07 -0700

    rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parent.java
@@ -88,7 +88,7 @@ abstract class Parent extends OpMode {
         }
 
         public void rotation(int pos, double pow){
-            setPosAll(pos, pos, pos, pos);
+            setPosAll(-pos, pos, -pos, pos);
 
             setPowerAll(pow);
 
