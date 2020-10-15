f6e1cb6c7a7ed12c92fbbca7dcf7cb44a1d977af
glftc3888 <greasedlightningrobotics@gmail.com>
2020-10-15 10:39:28 -0700

    launcher motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parent.java
@@ -12,6 +12,7 @@ abstract class Parent extends OpMode {
     DcMotor topRight;
     DcMotor bottomLeft;
     DcMotor bottomRight;
+
     ColorRangeSensor colorRangeSensor;
 
     public void init(){
