832cd8956d5d2db2445a7a118f3cc3fe00158f98
andre <andrewjalaimo@gmail.com>
2020-11-07 19:57:04 -0500

    Diagonal and sideways are swapped, Nathaniel bruh - AA, FB

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_1.java
@@ -1,13 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @Autonomous(group = "autonomous", name = "b")
 public class Auto_1 extends Parent {
     public void runOpMode() throws InterruptedException {
             initRobo();
             waitForStart();
-            fRbR(1000, .25);
+            fRbR(1700, -.125);
+            fRbR(500, 0);
+            rotation(1300, -.0625);
+            rotation(550, -.05);
+            rotation(1000,0);
+            diagonal(1000, -.125, -.125);
             // sideways(-200,.25);
 
         }
