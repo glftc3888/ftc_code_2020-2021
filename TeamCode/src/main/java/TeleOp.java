504033ebeaac473f12b855299e7a46495e98bc4e
GreasedLightning3888 <WoollyTM>
2020-11-21 16:02:30 -0500

    Movement code fixed, plus slow mode added, all servos added plus launcher PS your mom -Lucas

--- TeamCode/src/main/java/TeleOp.java
+++ TeamCode/src/main/java/TeleOp.java
@@ -7,19 +7,25 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp", group = "TeleOp")
 
 public class TeleOp extends OpMode {
+    ElapsedTime a = new ElapsedTime();
     DcMotor topLeft;
     DcMotor topRight;
     DcMotor bottomLeft;
     DcMotor bottomRight;
+    DcMotor intake;
+    DcMotor launch;
 
-    CRServo frontArm, backArm;
-    CRServo armServo; /* the */
+    Servo clawGrab;
+    Servo fPin;
+    CRServo rAP; /* the */
+    CRServo wrist;
 
-    DcMotor launcher;
+
     boolean on_off = false;
 
 
@@ -28,16 +34,21 @@ public class TeleOp extends OpMode {
         topRight = hardwareMap.dcMotor.get("topRightMotor");
         bottomLeft = hardwareMap.dcMotor.get("bottomLeftMotor");
         bottomRight = hardwareMap.dcMotor.get("bottomRightMotor");
+        intake = hardwareMap.dcMotor.get("intake");
+        launch = hardwareMap.dcMotor.get("launch");
 
-        frontArm = hardwareMap.crservo.get("front arm");
-        backArm = hardwareMap.crservo.get("back arm");
-        armServo = hardwareMap.crservo.get("arm servo");
+        clawGrab = hardwareMap.servo.get("grab");
+        wrist = hardwareMap.crservo.get("wrist");
+        rAP = hardwareMap.crservo.get("rAP");
+        fPin = hardwareMap.servo.get("fPin");
 
 
         topLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         topRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bottomLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bottomRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        launch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
 
@@ -45,10 +56,14 @@ public class TeleOp extends OpMode {
         topRight.setDirection(DcMotorSimple.Direction.FORWARD);
         bottomLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         bottomRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        launch.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        frontArm.setDirection(CRServo.Direction.FORWARD);
-        backArm.setDirection(CRServo.Direction.REVERSE);
-        armServo.setDirection(CRServo.Direction.FORWARD);
+
+        clawGrab.setDirection(Servo.Direction.FORWARD);
+        wrist.setDirection(CRServo.Direction.FORWARD);
+        rAP.setDirection(CRServo.Direction.FORWARD);
+        fPin.setDirection(Servo.Direction.FORWARD);
 
         topLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         topRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -62,9 +77,9 @@ public class TeleOp extends OpMode {
         bottomLeft.setPower(0);
         bottomRight.setPower(0);
 
-        backArm.setPower(0);
-        frontArm.setPower(0);
-        armServo.setPower(0);
+
+        rAP.setPower(0);
+        wrist.setPower(0);
 
 
     }
@@ -78,22 +93,34 @@ public class TeleOp extends OpMode {
     }
 */
     public void loop() {
-        topLeft.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
-        topRight.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
-        bottomLeft.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
-        bottomRight.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x);
-
         telemetry.addData("top left ", topLeft.getCurrentPosition());
         telemetry.addData("top right ", topRight.getCurrentPosition());
+        if(gamepad1.left_bumper) {
+            topLeft.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
+            topRight.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x);
+            bottomLeft.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+            bottomRight.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
+        }
+        topLeft.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)*0.25);
+        topRight.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x)*0.25);
+        bottomLeft.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)*0.25);
+        bottomRight.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x)*0.25);
+        rAP.setPower(-gamepad2.right_stick_y);
+        if (gamepad2.a)
+            clawGrab.setPosition(0);
+        if(gamepad2.b)
+            clawGrab.setPosition(1);
+        wrist.setPower(gamepad2.left_stick_x);
+            launch.setPower(gamepad2.right_trigger);
+            while(gamepad2.left_trigger==1){
+                fPin.setPosition(1);
+                fPin.setPosition(0);
+            }
+            fPin.setPosition(0);
+            intake.setPower(gamepad2.left_trigger);
 
 
-        backArm.setPower(gamepad2.left_stick_y);
-        frontArm.setPower(gamepad2.left_stick_y);
-        armServo.setPower(gamepad2.right_stick_y);
 
-
-        if (gamepad1.a);
-            //change();
 
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_1.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.Parent;
+
 @Autonomous(group = "autonomous", name = "b")
 public class Auto_1 extends Parent {
     public void runOpMode() throws InterruptedException {
